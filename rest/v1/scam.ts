2a85e5b1aba5ab22f8057fcce19bc52cffa4df22
Eydrenn <endreyestudos67@gmail.com>
2024-12-21 23:21:37 -0300

    refactor: scam rest

--- rest/v1/scam.ts
+++ rest/v1/scam.ts
@@ -1,4 +1,9 @@
-import type { APIPayload, APIScam } from "../../v1";
+import type {
+  APIAllScam,
+  APIPayload,
+  APIScam,
+  ISODateString,
+} from "../../v1";
 
 /**
  * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
@@ -10,4 +15,43 @@ export type RESTGetAPIScamBaseQuery = {
 /**
  * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
  */
+export interface RESTGetAPIScamPaginationQuery {
+	page?: number;
+	limit?: number;
+	skip?: number;
+	date_start?: ISODateString;
+	date_end?: ISODateString;
+}
+
+/**
+ * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
+ */
+export interface RESTGetAPIAllScamQuery
+	extends RESTGetAPIScamPaginationQuery {
+	guild_id?: string;
+}
+
+/**
+ * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
+ */
+export type RESTPostAPIScamCreateBody = APIScam;
+
+/**
+ * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
+ */
+export type RESTPatchAPIScamUpdateBody = Partial<APIScam>;
+
+/**
+ * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
+ */
 export type RESTGetAPIScamResponse = APIPayload<APIScam>;
+
+/**
+ * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
+ */
+export type RESTGetAPIScamHasResponse = APIPayload<boolean>;
+
+/**
+ * @see https://docs.quikcess.com/bet/api-reference/endpoint/scam
+ */
+export type RESTGetAPIAllScamResponse = APIPayload<APIAllScam>;
